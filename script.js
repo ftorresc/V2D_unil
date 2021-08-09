6dea9452c491b5d556991b363fbea70dc2a57df4
ftorresc <32510211+ftorresc@users.noreply.github.com>
2021-08-09 13:10:37 -0700

    add map

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log(d3.version)
+const svg = d3.select ('svg');
+
+const width = +svg.attr('width');
+const height = +svg.attr('height');
+
+const projection = d3.geoMercator();
+const pathGenerator = d3.geoPath().projection(projection);
+
+d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
+    .then(data => {
+        const countries = topojson.feature(data, data.objects.countries);
+        console.log(countries);
+
+        const paths = svg.selectAll('path')
+            .data(countries.features);
+        paths.enter().append('path')
+            .attr('d', d => pathGenerator(d));
+    });
