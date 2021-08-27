5adc5475702fb91352c8f1354f43bb52d09eb782
ftorresc <32510211+ftorresc@users.noreply.github.com>
2021-08-27 15:54:08 -0700

    add csv handling

--- scriptdata.js
+++ scriptdata.js
@@ -1,3 +1,16 @@
+let countryname = window.location.pathname.toString().replace("/", "").split(".")[0];
+
 d3.csv('travels.csv').then(data =>{
     console.log("Data",data)
+    for (var i= 0; i < data.length; i++) {
+        if (data[i].country.toString() == countryname) {
+            console.log(data[i].travels)
+            document.getElementById("title").innerHTML = data[i].country + "'s travel statistics during Euro 2020";
+            document.getElementById("stats").innerHTML = "Travels:" + "<br>" + data[i].travels + "<br><br>" + "Distance traveled during groups phase:" + data[i].distancegroups + " km" + "<br><br>" + "Total distance traveled during the tournament:" + data[i].distancetotal + " km";
+        }
+        else {
+            continue
+        }
+    }
 })
+
